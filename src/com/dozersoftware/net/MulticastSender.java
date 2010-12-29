501d84528aa955f98620ca5498e2e98394a75229
chrisangus <christopher.angus@l-3com.com>
2010-12-29 16:15:38 -0500

    Added reference to messaging for testing.  Also moved code for modularity.

--- src/com/dozersoftware/net/MulticastSender.java
+++ src/com/dozersoftware/net/MulticastSender.java
@@ -1,5 +1,7 @@
 package com.dozersoftware.net;
 
+import gov.nucas.messages.JAvPG1Msg;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
@@ -19,10 +21,12 @@ public class MulticastSender {
 			socket = new MulticastSocket();
 			//socket.setNetworkInterface(NetworkInterface.getByName(inf));
 			DatagramPacket dgram;
-			String test = "This is a test!";
-			byte[] msg = test.getBytes();
+			
+			//byte[] msg = BasicByteTest();
+			byte[] msg = PG1Test();
+			
 			dgram = new DatagramPacket(msg, msg.length,
-					InetAddress.getByName("224.0.0.6"), 6789);
+					InetAddress.getByName("239.150.100.75"), 55123);
 
 			System.out.println("Sending " + msg.length + " bytes to " +
 					dgram.getAddress() + ':' + dgram.getPort());
@@ -42,6 +46,30 @@ public class MulticastSender {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	private static byte[] BasicByteTest() {
+		String test = "This is a test!";
+		byte[] msg = test.getBytes();
+		msg[0] = 0x13;
+		msg[1] = (byte)0x96;
+		msg[14] = 0x03;
+		return msg;
+	}
+	
+	private static byte[] PG1Test() {
+		JAvPG1Msg oMsg = new JAvPG1Msg();
+		
+		byte[] ret = new byte[60];
+		
+		oMsg.Update();
+		oMsg.Set_ShipLat(34.0);
+		
+		
+		oMsg.ToBuffer(ret);
+		
+		
+		return ret;
 	}
 
 }
